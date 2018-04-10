66362c77ee7a56a605200bb7f0b86526a7e9cc58
Vinicius <vs.almeida@icloud.com>
2018-04-10 12:25:20 -0300

    alteraçoes feitas 12:25

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,9 @@ import java.util.Optional;
 @Controller
 public class SalaController {
 
+	@Autowired
+	private SessaoDao sessaoDao;
+	
     @Autowired
     private SalaDao salaDao;
 
@@ -66,6 +70,7 @@ public class SalaController {
         Sala sala = salaDao.findOne(id);
 
         ModelAndView view = new ModelAndView("sessao/lista");
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
         view.addObject("sala", sala);
 
         return view;
